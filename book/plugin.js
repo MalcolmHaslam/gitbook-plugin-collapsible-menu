8acb2e4d74d6be485dc33135a5517ea27d73d99a
MalcolmHaslam <mhaslam@runmyprocess.com>
2017-04-04 17:09:33 +0200

    Update plugin.js

--- book/plugin.js
+++ book/plugin.js
@@ -1,8 +1,9 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
-        $('ul.summary li li').hide();
-        // $('ul.summary li').find('li.active').parent().children().show();
-        $('ul.summary li li.active').parents().children(':not(script)').show();
-        $('ul.summary li.active > ul > li').show();        
+        $('ul.summary li').hide();
+		$('li.active').parents().show();
+		$('li.active').parents("ul").not("ul.summary").children().show();
+		$('li.active').show();
+		$('li.active').children("ul").children().show();   
     });
 });
